f050be283feb0a351f92630f77a8bbde063172fd
Sergio Christian Siccha <sergiocf@amazon.de>
2022-12-02 14:05:24 +0100

    day1 task1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,13 +1,30 @@
 use std::fs;
 
 fn main() {
-    println!("Hello, Sergio!");
-    let _input = fs::read_to_string("input")
+    let input = fs::read_to_string("input")
         .expect("Failed to read input!");
+    let list_of_calories = input.lines();
 
-    let list_of_calories = _input.lines();
-    let nr_cals = list_of_calories.count();
-    println!("len list {nr_cals}");
-    let nr: i32 = "1".parse().unwrap();
-    println!("You guessed {nr}");
+    let mut total_calories_current_elf = 0;
+    let mut total_calories_best_elf = 0;
+
+    for line in list_of_calories {
+        // a new elf
+        if line.is_empty() {
+            if total_calories_current_elf > total_calories_best_elf {
+                total_calories_best_elf = total_calories_current_elf;
+            }
+            total_calories_current_elf = 0;
+            continue
+        }
+        let number: u64 = line.parse().unwrap();
+        // update calories
+        total_calories_current_elf += number;
+        // update highest_calories and best_elf
+    }
+    println!("{total_calories_best_elf}");
 }
+
+// track calories of top three elves. Return some of those calories
+// https://doc.rust-lang.org/std/collections/struct.BinaryHeap.html
+// A priority queue implemented with a binary heap.
